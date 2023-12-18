73e673e905a714dc7b5e669a14b9e808f1a53fb2
Setu Gupta <setu@tamu.edu>
2023-12-18 16:23:58 -0600

    LFU fixed (maybe)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   replacement/lfu/lfu.cc

--- replacement/lfu/lfu.cc
+++ replacement/lfu/lfu.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
+#include <map>
 
 #include "cache.h"
 
@@ -9,106 +10,94 @@
 
 namespace LFU_Policy
 {
-	class LFU
-	{
-		private:
-			std::vector<uint32_t> frequencies;
-			uint32_t num_ways = 0;
+        class LFU
+        {
+                private:
+                        std::vector<uint32_t> frequencies;
 
-		public:
-			LFU()
-			{
-				this->num_ways = 0;
-			}
+                public:
+                        LFU(uint32_t ways)
+                        {
+                                this->frequencies = std::vector<uint32_t>(ways);
+                        }
 
-			void set_ways(uint32_t ways)
-			{
-				this->num_ways = ways;
+                        void increment(uint32_t way)
+                        {
+                                // Run sanity check
+                                assert(way < this->frequencies.size());
 
-				uint32_t idx = 0;
-				for(idx = 0; idx < this->num_ways; idx++)
-					this->frequencies.emplace_back(0);
-			}
+                                // Get the maximum frequency
+                                uint32_t max_freq = *std::max_element(this->frequencies.begin(), this->frequencies.end());
 
-			void increment(uint32_t way)
-			{
-				// Run sanity check
-				assert(way < this->num_ways);
+                                // Divide the frequencies by 2 if the maximum frequency exceeds MAX_FREQ
+                                if(max_freq >= MAX_FREQ)
+                                {
+                                        std::vector<uint32_t>::iterator it;
+                                        for(it = this->frequencies.begin(); it != this->frequencies.end(); it++)
+                                                *it >>= 1;
+                                }
 
-				// Get the maximum frequency
-				uint32_t max_freq = *std::max_element(this->frequencies.begin(), this->frequencies.end());
+                                this->frequencies[way]++;
+                        }
 
-				// Divide the frequencies by 2 if the maximum frequency exceeds MAX_FREQ
-				if(max_freq >= MAX_FREQ)
-				{
-					std::vector<uint32_t>::iterator it;
-					for(it = this->frequencies.begin(); it != this->frequencies.end(); it++)
-						*it >>= 1;
-				}
+                        uint32_t get_lfu()
+                        {
+                                std::vector<uint32_t>::iterator min_way = std::min_element(this->frequencies.begin(), this->frequencies.end());
+                                return (uint32_t)(std::distance(this->frequencies.begin(), min_way));
+                        }
+        };
 
-				this->frequencies[way]++;
-			}
-
-			uint32_t get_lfu()
-			{
-				std::vector<uint32_t>::iterator min_way = std::min_element(this->frequencies.begin(), this->frequencies.end());
-				return (uint32_t)(std::distance(this->frequencies.begin(), min_way));
-			}
-	};
+        std::map<CACHE*, std::vector<LFU>> policies;
 }
-
-LFU_Policy::LFU* policies;
 
 void CACHE::initialize_replacement()
 {
-	policies = new LFU_Policy::LFU[NUM_SET]();	// Create the LFU policies for all the cache sets
+        LFU_Policy::policies[this] = std::vector<LFU_Policy::LFU>();
 
-	// Set the number of ways for all sets
-	for(uint32_t i = 0; i < NUM_SET; i++)
-		policies[i].set_ways(NUM_WAY);
+        // Allocate the policies, one per cache set
+        for(uint32_t idx = 0; idx < NUM_SET; idx++)
+                LFU_Policy::policies[this].emplace_back(NUM_WAY);
 }
 
 uint32_t CACHE::find_victim(
-		[[maybe_unused]] uint32_t triggering_cpu,
-		[[maybe_unused]] uint64_t instr_id,
-		uint32_t set,
-		[[maybe_unused]] const BLOCK* current_set,
-		[[maybe_unused]] uint64_t ip,
-		[[maybe_unused]] uint64_t full_addr,
-		[[maybe_unused]] uint32_t type
-		)
+                [[maybe_unused]] uint32_t triggering_cpu,
+                [[maybe_unused]] uint64_t instr_id,
+                uint32_t set,
+                [[maybe_unused]] const BLOCK* current_set,
+                [[maybe_unused]] uint64_t ip,
+                [[maybe_unused]] uint64_t full_addr,
+                [[maybe_unused]] uint32_t type
+                )
 {
-	// Run sanity check
-	assert(set < NUM_SET);
+        // Run sanity check
+        assert(set < LFU_Policy::policies[this].size());
 
-	// Return the LFU way
-	return policies[set].get_lfu();
+        // Return the LFU way
+        return LFU_Policy::policies[this][set].get_lfu();
 }
 
 void CACHE::update_replacement_state(
-		[[maybe_unused]] uint32_t triggering_cpu,
-		uint32_t set,
-		uint32_t way,
-		[[maybe_unused]] uint64_t full_addr,
-		[[maybe_unused]] uint64_t ip,
-		[[maybe_unused]] uint64_t victim_addr,
-		[[maybe_unused]] uint32_t type,
-		[[maybe_unused]] uint8_t hit
-		)
+                [[maybe_unused]] uint32_t triggering_cpu,
+                uint32_t set,
+                uint32_t way,
+                [[maybe_unused]] uint64_t full_addr,
+                [[maybe_unused]] uint64_t ip,
+                [[maybe_unused]] uint64_t victim_addr,
+                [[maybe_unused]] uint32_t type,
+                [[maybe_unused]] uint8_t hit
+                )
 {
-	// Run sanity checks
-	assert(way < NUM_WAY);
-	assert(set < NUM_SET);
+        // Run sanity checks
+        assert(way < NUM_WAY);
+        assert(set < LFU_Policy::policies[this].size());
 
-	// Increment frequency
-	if(!hit)	// Only update the state on cache fills
-		policies[set].increment(way);
+        // Increment frequency
+        if(!hit)        // Only update the state on cache fills
+                LFU_Policy::policies[this][set].increment(way);
 }
 
 void CACHE::replacement_final_stats()
 {
-	delete[] policies;
-
-	// Nothing needs to be done
-	return;
+        // Do nothing
+        return;
 }
