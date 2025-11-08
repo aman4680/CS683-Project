4e733c35fcc08c3b5b9f94638432d966cae30ad2
aman4680 <amanpratapsingh952@gmail.com>
2025-11-08 12:10:16 +0530

    Commit message
    - Created Alderlake JSON for LRU, SRRIP, DRRIP, SHIP and DUEL-PACIPV
    - Added replacement/duel_ipv/duel_ipv.cc file
    - Added SOTA_configs/tests/replacement_policies/duel_IPV.json
    - Organized results directory
    - Organised background logs into output_logs directory
    - Modified run*.sh files
    - Deleted old CSV data for LRU and PACIPV
    - You need to modify extract_data_for_lru.py and extract_data_for_pacipv.py due to new results directory structure

--- run.sh
+++ run.sh
@@ -9,7 +9,8 @@ L1I_IPV=1_1_1_2_4#1_1_2_2_4 \
 # OR 
 
 
-
+In background
+# For pacipv:
 nohup env \
 L1I_IPV=1_1_1_2_4#1_1_2_2_4 \
 L1D_IPV=1_1_1_2_4#1_1_2_2_4 \
@@ -17,4 +18,19 @@ L2C_IPV=1_1_1_2_4#1_1_2_2_4 \
 LLC_IPV=1_1_1_2_4#1_1_2_2_4 \
 ./bin/champsim_test_PACIPV --warmup-instructions 200000000 \
 --simulation-instructions 500000000 ../traces/public_traces/compute_int/compute_int_5.champsimtrace.xz \
+> results/output.txt 2>&1 &
+
+
+
+# For duel:
+nohup env \
+L1I_IPV=1_1_1_2_4#1_1_2_2_4 \
+L1D_IPV=1_1_1_2_4#1_1_2_2_4 \
+L2C_IPV=1_1_1_2_4#1_1_2_2_4 \
+LLC_IPV_INSTR=1_2_2_1_4#1_2_1_1_4 \
+LLC_IPV_DATA=1_1_1_2_4#1_1_2_2_4 \
+./bin/champsim_test_DUEL_IPV \
+--warmup-instructions 200000000 \
+--simulation-instructions 500000000 \
+../traces/public_traces/compute_int/compute_int_5.champsimtrace.xz \
 > results/output.txt 2>&1 &
